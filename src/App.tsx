92efcda3cf9434a425774114325615b25b1603e0
Mermersk <ic4ruz39@gmail.com>
2019-09-05 20:22:52 +0200

    Learned more about Typescript and React

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 //Import our stylesheet to the entrypoint of our application/website.
 import "./App.css";
 //Importing my own custom made Components
-import SayHello from "./sayHello";
-import Tweet from "./Tweet"
+import SayHello from "./components/sayHello";
+import Tweet from "./components/Tweet";
+import NewTaskForm from "./components/NewTaskForm"
+import ClassComponent from "./components/ClassComponent"
 
 const App: React.FC = () => {
 
@@ -21,8 +23,7 @@ const App: React.FC = () => {
       <SayHello/>
       {/* We can define and add our own attributes to our components!*/}
       <Tweet name = "Isak" message = "I am learning React"/>
-      <Tweet name = "Jon" message = "I am Johhnyboi"/>
-      <Tweet name = "Magnus" message = "The hammer of Thor"/>
+      <ClassComponent/>
     </div>
   );
 }
---
+++ src/models/task.ts
@@ -0,0 +1,7 @@
+//An Typescript interface is an contract between it and the objects/classes that implement it.
+export interface Task {
+
+    id: number;
+    name: string;
+}
+
